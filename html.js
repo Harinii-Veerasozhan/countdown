adc8b49dd61169eda26eb6bf78d06750ecca5afc
Harinii-Veerasozhan <hariniiveerasozhan@gmail.com>
2025-05-16 02:36:42 +0530

    first commit

---
+++ html.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    console.log(document.getElementById("count").innerHTML=10)
+    setTimeout(()=>{
+        console.log(document.getElementById("count").innerHTML=9)
+        setTimeout(()=>{
+            console.log(document.getElementById("count").innerHTML=8)
+            setTimeout(()=>{
+                console.log(document.getElementById("count").innerHTML=7)
+                setTimeout(()=>{
+                    console.log(document.getElementById("count").innerHTML=6)
+                    setTimeout(()=>{
+                        console.log(document.getElementById("count").innerHTML=5)
+                        setTimeout(()=>{
+                            console.log(document.getElementById("count").innerHTML=4)
+                            setTimeout(()=>{
+                                console.log(document.getElementById("count").innerHTML=3)
+                                setTimeout(()=>{
+                                    console.log(document.getElementById("count").innerHTML=2)
+                                    setTimeout(()=>{
+                                        console.log(document.getElementById("count").innerHTML=1)
+                                        setTimeout(()=>{
+                                            console.log(document.getElementById("count").innerHTML="HAPPY BIRTHDAY")
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
